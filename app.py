87347ef5b3283d37bb1b06336c4c33ca67a64f4d
Javier Blanco <javierblancoit@gmail.com>
2022-07-13 20:32:54 +0100

    create student resource for restful api

--- app.py
+++ app.py
@@ -3,3 +3,14 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+
+
+# Apis work with resources where each resource is a class
+
+class Student(Resource):
+    def get(self, name):
+        return {'student': name}
+
+api.add_resource(Student, '/student/<string:name>') # http://localhost/student/rolf
+
+app.run(port=5000)
