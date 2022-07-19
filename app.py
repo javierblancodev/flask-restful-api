3e064f10d1cc5d534ad181baf7da2ac6ba91080e
Javier Blanco <javierblancoit@gmail.com>
2022-07-19 19:16:26 +0100

    create resource for the items list

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
+from distutils.log import debug
 from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+items = []
 
 # Apis work with resources where each resource is a class
+# We no longer need to use jsonify when working with flask_restful because this library features will do it for us
 
-class Student(Resource):
+# Resource is a class itself (it comes with some methods like get, post, etc.) that can be extended so we can create our own resource
+class Item(Resource):
+    # The name parameter refer/comes directly to/from the slug in the url
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item["name"] == name:
+                return item
+        return {"message": "Sorry, no matching item has been found"}, 404
 
-api.add_resource(Student, '/student/<string:name>') # http://localhost/student/rolf
+    def post(self, name):
+        item = {"name": name, "price": 12.99}
+        items.append(item)
+        return item, 201
 
-app.run(port=5000)
+class ItemsList(Resource):
+    def get(self):
+        return {'items': items}
+
+# Add the Item resource to our app 
+# The add_resource method from the API object will take two arguments: the resource and the url
+api.add_resource(Item, '/item/<string:name>') # http://localhost/item/chair
+api.add_resource(ItemsList, '/items')
+
+app.run(port=5000, debug=True) # Debug will return a nice error message in case that the program enconter any
